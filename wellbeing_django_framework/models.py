6603350b7024f2975c4030818ed986421dd9851c
401078858@qq.com <q1w2e3r4>
2023-03-24 18:33:49 +0800

    add Django Cryptography to encrypt fields.

--- wellbeing_django_framework/models.py
+++ wellbeing_django_framework/models.py
@@ -4,6 +4,7 @@ from pygments.styles import get_all_styles
 from pygments.lexers import get_lexer_by_name
 from pygments.formatters.html import HtmlFormatter
 from pygments import highlight
+from django_cryptography.fields import encrypt
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
@@ -11,14 +12,14 @@ STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
 
 
 class Snippet(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    title = models.CharField(max_length=100, blank=True, default='')
-    code = models.TextField()
-    linenos = models.BooleanField(default=False)
-    language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
-    style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
+    created = encrypt(models.DateTimeField(auto_now_add=True))
+    title = encrypt(models.CharField(max_length=100, blank=True, default=''))
+    code = encrypt(models.TextField())
+    linenos = encrypt(models.BooleanField(default=False))
+    language = encrypt(models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100))
+    style = encrypt(models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100))
     owner = models.ForeignKey('auth.User', related_name='snippets', on_delete=models.CASCADE)
-    highlighted = models.TextField()
+    highlighted = encrypt(models.TextField())
 
     def save(self, *args, **kwargs):
         """
