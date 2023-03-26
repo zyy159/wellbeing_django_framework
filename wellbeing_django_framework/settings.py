115b2cb935cfe2e23d06e6c9137b25aa3f181f35
401078858@qq.com <q1w2e3r4>
2023-03-26 20:08:57 +0800

    fix some issue

--- wellbeing_django_framework/settings.py
+++ wellbeing_django_framework/settings.py
@@ -118,7 +118,7 @@ REST_FRAMEWORK = {
     #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     # ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        # 'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
