f7225adaa2856185dd8adaf6b0f92b4436259cf8
401078858@qq.com <q1w2e3r4>
2023-05-27 16:32:55 +0800

    add allowed host, switch DB to sqlite3

--- wellbeing_django_framework/settings.py
+++ wellbeing_django_framework/settings.py
@@ -42,7 +42,7 @@ LOGGING = {
 }
 
 
-ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost', '54.237.200.39']
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost', '121.43.148.79']
 
 CSRF_TRUSTED_ORIGINS = ['127.0.0.1', '0.0.0.0', 'localhost']
 
@@ -106,22 +106,22 @@ WSGI_APPLICATION = 'wellbeing_django_framework.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgreWB',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgreWB',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
