7dcfb710e2227461e007939c6dfcf62febbf92a2
Stanven-1987 <79982058+Stanven-1987@users.noreply.github.com>
2023-06-25 11:51:54 +0800

    Refactor Models

--- wellbeing_django_framework/exercise/admin.py
+++ wellbeing_django_framework/exercise/admin.py
@@ -2,30 +2,32 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-class MotionAdmin(admin.ModelAdmin):
-    list_display = ("motion_name", "motion_type", "motion_description", "motion_created", "motion_demo",
-                    "motion_ready", "motion_popularity", "motion_model_type", "motion_model")
-    list_filter = ("motion_type", "motion_ready")
-
-
-class WorkoutAdmin(admin.ModelAdmin):
-    list_display = ("user", "event", "start_time", "end_time", "label", "score", "calories", "status")
-    list_filter = ("user", "label")
-
-
-class Model_storeAdmin(admin.ModelAdmin):
-    list_display = ("model_name", "model_type", "model_store_url", "created", "updated", "model_version")
-    list_filter = ("model_name", "model_type")
-
-class EventAdmin(admin.ModelAdmin):
-    list_display = (
-    "user", "event_name", "motion_description", "event_motions", "schedule", "status", "created", "updated",
-    "event_start_time", "event_end_time", "event_location", "event_version")
-    list_filter = ("user", "event_start_time", "event_end_time")
-
-class VerificationAdmin(admin.ModelAdmin):
-    list_display = ("user", "ver_url", "ver_status","expire_time")
-    list_filter = ("user",)
+# class MotionAdmin(admin.ModelAdmin):
+#     list_display = ("motion_name", "motion_type", "motion_description", "motion_created", "motion_model_url",
+#                     "motion_start","motion_end","motion_version",
+#                     "motion_demo",
+#                     "motion_ready", "motion_popularity", "motion_model_type")
+#     list_filter = ("motion_type", "motion_ready")
+#
+#
+# class WorkoutAdmin(admin.ModelAdmin):
+#     list_display = ("user", "event", "start_time", "end_time", "label", "score", "calories", "status")
+#     list_filter = ("user", "label")
+#
+# # class Model_storeAdmin(admin.ModelAdmin):
+# #     list_display = ("model_name", "model_type", "model_store_url", "created", "updated", "model_version")
+# #     list_filter = ("model_name", "model_type")
+#
+# class EventAdmin(admin.ModelAdmin):
+#     list_display = (
+#     "user", "event_name", "motion_description", "event_motions", "schedule", "status", "created", "updated",
+#     "event_start_time", "event_end_time", "event_location", "event_version")
+#     list_filter = ("user", "event_start_time", "event_end_time")
+#
+#
+# # class VerificationAdmin(admin.ModelAdmin):
+# #     list_display = ("user", "ver_url", "ver_status","expire_time")
+# #     list_filter = ("user",)
 
 class Wellbeing_userAdmin(admin.ModelAdmin):
     list_display = (
@@ -38,12 +40,31 @@ class Resource_storeAdmin(admin.ModelAdmin):
         list_filter = ("resource_name",)
 
 
-admin.site.register(Motion, MotionAdmin)
-admin.site.register(Model_store, Model_storeAdmin)
-admin.site.register(Workout, WorkoutAdmin)
+class ExerciseAdmin(admin.ModelAdmin):
+    list_display = (
+    "name", "popularity", "start_time", "end_time")
+    list_filter = ("name",)
 
-admin.site.register(Event, EventAdmin)
-admin.site.register(Verification, VerificationAdmin)
+class ActionAdmin(admin.ModelAdmin):
+    list_display = ("name","exercise", "popularity", "start_time", "end_time", "image_url")
+    list_filter = ("name",)
+
+class Model_storeAdmin(admin.ModelAdmin):
+    list_display = ("name", "exercise", "model_url", "created", "updated", "version")
+    list_filter = ("name",)
+
+class ScheduleAdmin(admin.ModelAdmin):
+    list_display = ("user","name", "exercise","date", "exercise", "start_time", "end_time", "content")
+    list_filter = ("name",)
+
+# class Plan(admin.ModelAdmin):
+#     list_display = (
+#     "plan_name", "plan_start", "plan_end", "plan_event")
+#     list_filter = ("plan_name",)
+
+admin.site.register(Exercise, ExerciseAdmin)
+admin.site.register(Model_store, Model_storeAdmin)
+admin.site.register(Action, ActionAdmin)
+admin.site.register(Schedule, ScheduleAdmin)
 admin.site.register(Wellbeing_user, Wellbeing_userAdmin)
 admin.site.register(Resource_store, Resource_storeAdmin)
-
